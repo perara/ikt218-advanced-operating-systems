5bc4fadb8505ff088bed480a416d80ff1b112372
Per-Arne Andersen <per@sysx.no>
2022-12-31 22:50:59 +0100

    IDT - Implement Interrupt Descriptor Table

---
+++ src/cpu/i386/idt.h
@@ -0,0 +1,31 @@
+//
+// Created by per on 12/31/22.
+//
+
+#ifndef UIAOS_IDT_H
+#define UIAOS_IDT_H
+#include "../../../cmake-build-debug/_deps/musl-build/include/stdint.h"
+
+// A struct describing an interrupt gate.
+struct idt_entry_struct
+{
+    uint16_t base_lo;             // The lower 16 bits of the address to jump to when this interrupt fires.
+    uint16_t sel;                 // Kernel segment selector.
+    uint8_t  always0;             // This must always be zero.
+    uint8_t  flags;               // More flags. See documentation.
+    uint16_t base_hi;             // The upper 16 bits of the address to jump to.
+} __attribute__((packed));
+typedef struct idt_entry_struct idt_entry_t;
+
+// A struct describing a pointer to an array of interrupt handlers.
+// This is in a format suitable for giving to 'lidt'.
+struct idt_ptr_struct
+{
+    uint16_t limit;
+    uint32_t base;                // The address of the first element in our idt_entry_t array.
+} __attribute__((packed));
+typedef struct idt_ptr_struct idt_ptr_t;
+
+void init_idt();
+
+#endif //UIAOS_IDT_H
