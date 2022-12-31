ecfdc2d77d6640065970cdd1fa68ed6498c9bd42
Per-Arne Andersen <per@sysx.no>
2022-12-31 22:56:43 +0100

    CPU - Include toolkit for interacting with low level interfaces such as input/output devices

---
+++ src/cpu/i386/hardware.h
@@ -0,0 +1,12 @@
+//
+// Created by per on 12/31/22.
+//
+
+#ifndef UIAOS_HARDWARE_H
+#define UIAOS_HARDWARE_H
+
+#include "hardware_port.h"
+#include "isr.h"
+
+
+#endif //UIAOS_HARDWARE_H
---
+++ src/cpu/i386/hardware_port.h
@@ -0,0 +1,29 @@
+//
+// Created by per on 12/31/22.
+//
+
+#ifndef UIAOS_HARDWARE_PORT_H
+#define UIAOS_HARDWARE_PORT_H
+#include <stdint.h>
+
+
+#pragma once
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void outb(uint16_t port, uint8_t value);
+uint8_t inb(uint16_t port);
+uint16_t inw(uint16_t port);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+
+
+#endif //UIAOS_HARDWARE_PORT_H
