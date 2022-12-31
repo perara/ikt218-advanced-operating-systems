16a5d741dc2ae43801688164fedfb268f817a39d
Per-Arne Andersen <per@sysx.no>
2022-12-31 22:30:06 +0100

    KERNEL - Create our own stdlib for non-standard functions in the c library!

--- src/kernel/src/system/main.cpp
+++ src/kernel/src/system/main.cpp
@@ -1,10 +1,16 @@
 //
 // Created by per on 12/30/22.
 //
+#include "uiastdlib.h"
+
+
 
 extern "C" void kernel_main()
 {
-    char* video_memory = (char*) 0xb8000;
-    *video_memory = 3; // Should print a heart
+    // Now we can use itoa!
+    char str_num[1];
+    itoa(9, str_num);
 
+    char* video_memory = (char*) 0xb8000;
+    *video_memory = str_num[0]; // Should print 9
 }
